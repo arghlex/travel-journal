c548abd100ef2644540743b707d21de72f023fa5
Alex Mellor <alex.mellor@gmail.com>
2022-09-16 16:49:54 +0100

    Add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        
+        <header className="header">
+            <i class="fa-solid fa-earth-americas"></i>
+            <h1>My Travel Journal.</h1>
+        </header>
+
+    )
+}
